6b896335d620f9d3529d23a8301e5962fde8fded
ZZzyc <60928623+ZZzyc001@users.noreply.github.com>
2020-04-19 12:00:27 +0800

    Create 01 -- usegolf.cpp

---
+++ ch9/01 -- usegolf.cpp
@@ -0,0 +1,31 @@
+// usegolf.cpp
+
+#include <iostream>
+#include "golf.h"
+using namespace std;
+
+int main()
+{
+	golf g1, g2[5];
+
+	cout << "Using setgolf() with entered value:\n";
+	setgolf(g1, "I'm a people", 20);
+	showgolf(g1);
+
+	cout << "Using setgolf() with solicitation:\n";
+	int n = 0;
+	while (n < 5)
+		if (!setgolf(g2[n++]))
+			break;
+	--n;
+	
+	cout << "Using handicap() to revalue g2:\n";
+	for (int i = 0; i < n; ++i)
+		handicap(g2[i], 10 * i);
+
+	cout << "Using showgolf() to show g2:\n";
+	for (int i = 0; i < n; ++i)
+		showgolf(g2[i]);
+
+	return 0;
+}
