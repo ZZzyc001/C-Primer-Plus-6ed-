15aed7fd5810b2212df4932f7dfbb78093bf47c6
ZZzyc <60928623+ZZzyc001@users.noreply.github.com>
2020-04-25 10:05:06 +0800

    Create 03 -- golf.h

---
+++ ch10/03 -- golf.h
@@ -0,0 +1,19 @@
+// golf.h
+#ifndef GOLF_H_
+#define GOLF_H_
+
+class Golf
+{
+private:
+	static const int Len = 40;
+	char fullname[Len];
+	int handicap;
+public:
+	Golf() {}
+	Golf(const char* name, int hc);
+	int setgolf();
+	void sethandicap(int hc);
+	void showgolf() const;
+};
+
+#endif
