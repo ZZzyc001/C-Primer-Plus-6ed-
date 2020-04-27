29fe58e75d62c2320b3f6d71f2103c32688e452d
ZZzyc <60928623+ZZzyc001@users.noreply.github.com>
2020-04-27 19:08:35 +0800

    Create 06 -- stonewt.h

---
+++ ch11/06 -- stonewt.h
@@ -0,0 +1,25 @@
+// stonewt.h
+#ifndef STONEWT_H_
+#define STONEWT_H_
+class Stonewt
+{
+private:
+	enum { Lbs_per_stn = 14 };
+	int stone;
+	double pds_left;
+	double pounds;
+public:
+	Stonewt(double lbs);
+	Stonewt(int stn, double lbs);
+	Stonewt();
+	~Stonewt();
+	bool operator> (const Stonewt& st) const;
+	bool operator< (const Stonewt& st) const;
+	bool operator== (const Stonewt& st) const;
+	bool operator!= (const Stonewt& st) const;
+	bool operator>= (const Stonewt& st) const;
+	bool operator<= (const Stonewt& st) const;
+	void show_lbs() const;
+	void show_stn() const;
+};
+#endif
