81357ee66ab5dffead97f3d81e9b4dd9bd932ae2
ZZzyc <60928623+ZZzyc001@users.noreply.github.com>
2020-05-04 16:46:36 +0800

    Create 01 --  cow.cpp

---
+++ ch12/01 --  cow.cpp
@@ -0,0 +1,53 @@
+// cow.cpp
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include "cow.h"
+
+Cow::Cow()
+{
+	name[0] = '\0';
+	hobby = nullptr;
+	weight = 0;
+}
+
+Cow::Cow(const char* nm, const char* ho, double wt)
+{
+	strcpy(name, nm);
+	hobby = new char[strlen(ho) + 1];
+	strcpy(hobby, ho);
+	weight = wt;
+}
+
+Cow::Cow(const Cow& c)
+{
+	strcpy(name, c.name);
+	hobby = new char[strlen(c.hobby) + 1];
+	strcpy(hobby, c.hobby);
+	weight = c.weight;
+}
+
+Cow::~Cow()
+{
+	delete[] hobby;
+}
+
+Cow& Cow::operator=(const Cow& c)
+{
+	if (this == &c)
+		return *this;
+	strcpy(name, c.name);
+	delete[] hobby;
+	hobby = new char[strlen(c.hobby) + 1];
+	strcpy(hobby, c.hobby);
+	weight = c.weight;
+}
+
+void Cow::ShowCow() const
+{
+	using std::cout;
+	using std::endl;
+	cout << "Name: " << name << endl;
+	cout << "Hobby: " << hobby << endl;
+	cout << "Weight: " << weight << endl;
+}
