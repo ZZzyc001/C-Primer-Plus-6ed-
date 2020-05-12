d6f6181a321c2d3ec1260b411d759e153bbcd123
ZZzyc <60928623+ZZzyc001@users.noreply.github.com>
2020-05-12 17:54:57 +0800

    Create pe14-1.cpp

---
+++ ch14/pe14-1.cpp
@@ -0,0 +1,31 @@
+// pe14-1.cpp
+#include <iostream>
+#include "winec.h"
+
+int main()
+{
+	using std::cin;
+	using std::cout;
+	using std::endl;
+
+	cout << "Enter name of wine: ";
+	char lab[50];
+	cin.getline(lab, 50);
+	cout << "Enter number of years: ";
+	int yrs;
+	cin >> yrs;
+
+	Wine holding(lab, yrs);
+	holding.GetBottles();
+	holding.Show();
+
+	const int YRS = 3;
+	int y[YRS] = { 1993, 1995, 1998 };
+	int b[YRS] = { 48, 60, 72 };
+	Wine more("Gushing Grape Red", YRS, y, b);
+	more.Show();
+	cout << "Total bottles for " << more.Lable()
+		<< ": " << more.sum() << endl;
+	cout << "Bye\n";
+	return 0;
+}
