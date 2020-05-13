79da5c85e63caad4ccfdc23846fcff17f247b850
ZZzyc <60928623+ZZzyc001@users.noreply.github.com>
2020-05-13 21:22:28 +0800

    Create 04 -- main.cpp

---
+++ ch14/04 -- main.cpp
@@ -0,0 +1,58 @@
+// main.cpp
+#include <iostream>
+#include <cstring>
+#include "person.h"
+const int SIZE = 5;
+
+int main()
+{
+	using std::cin;
+	using std::cout;
+	using std::endl;
+	using std::strchr;
+
+	Person* lolas[SIZE];
+
+	int ct;
+	for (ct = 0; ct < SIZE; ++ct)
+	{
+		char choice;
+		cout << "Enter the employee category:\n"
+			<< "g: gunslinger  p: pokerplayer  "
+			<< "b: bad dude  q: quit\n";
+		cin >> choice;
+		while (strchr("gpbq", choice) == NULL)
+		{
+			cout << "Please enter a g, p, b, or q: ";
+			cin >> choice;
+		}
+		if (choice == 'q')
+			break;
+		switch (choice)
+		{
+		case 'g':
+			lolas[ct] = new Gunslinger;
+			break;
+		case 'p':
+			lolas[ct] = new PokerPlayer;
+			break;
+		case 'b':
+			lolas[ct] = new BadDude;
+			break;
+		}
+		cin.get();
+		lolas[ct]->Set();
+	}
+
+	cout << "\nHere is the person:\n";
+	int i;
+	for (i = 0; i < ct; ++i)
+	{
+		cout << endl;
+		lolas[i]->Show();
+	}
+	for (int i = 0; i < ct; ++i)
+		delete lolas[i];
+	cout << "Bye.\n";
+	return 0;
+}
